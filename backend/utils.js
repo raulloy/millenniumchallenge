511b552b03266d69d36aac959e156487ee613bdd
RaulLoyola <raul.loy@gmail.com>
2024-11-25 00:56:02 -0600

    fixing baseUrl

--- backend/utils.js
+++ backend/utils.js
@@ -252,8 +252,4 @@ export const saveAggregatedSurvey = async (surveyData) => {
 };
 
 export const baseUrl = () =>
-  process.env.BASE_URL
-    ? process.env.BASE_URL
-    : process.env.NODE_ENV !== "production"
-      ? "http://localhost:3000"
-      : "https://yourdomain.com";
+  process.env.BASE_URL ? process.env.BASE_URL : "http://localhost:3000";
